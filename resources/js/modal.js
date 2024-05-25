769d527733afccc341b91c377d7414852891ff2d
andyanonopv <abodogai@yahoo.com>
2024-05-25 20:12:42 +0300

    restructured code

--- resources/js/modal.js
+++ resources/js/modal.js
@@ -1,4 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
+    
+    // Elements for create modal
+    const createButton = document.getElementById('createButton');
+    const closeButton = document.querySelector('.closeBtn');
+    const modalCreate = document.querySelector('.modalCreate');
+
+    // Elements for edit modals
+    const editButtons = document.querySelectorAll('.editBtn');
+    const closeEditButtons = document.querySelectorAll('.closeEditButton');
+    const editModals = document.querySelectorAll('.modalEdit');
+
+    // Elements for delete modals
+    const deleteButtons = document.querySelectorAll('.removeBtn');
+    const closeDeleteButtons = document.querySelectorAll('.closeRemoveBtn');
+    const deleteModals = document.querySelectorAll('.modalDelete');
+    
+    // Function to handle multiple buttons and corresponding modals
+    function handleMultipleButtons(buttons, closeButtons, modals) {
+        buttons.forEach((button, index) => {
+            let modal = modals[index];
+            let closeButton = closeButtons[index];
+            handleSingleButton(button, closeButton, modal);
+        });
+    }
+
+    
     // Function to handle single button and modal pair
     function handleSingleButton(button, closeButton, modal) {
         button.addEventListener('click', () => {
@@ -11,37 +37,17 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    // Function to handle multiple buttons and corresponding modals
-    function handleMultipleButtons(buttons, closeButtons, modals) {
-        buttons.forEach((button, index) => {
-            let modal = modals[index];
-            let closeButton = closeButtons[index];
-            handleSingleButton(button, closeButton, modal);
-        });
-    }
-
-    // Elements for create modal
-    const createButton = document.getElementById('createButton');
-    const closeButton = document.querySelector('.closeBtn');
-    const modalCreate = document.querySelector('.modalCreate');
-
+    
+    
     if (createButton && closeButton && modalCreate) {
         handleSingleButton(createButton, closeButton, modalCreate);
     }
 
-    // Elements for edit modals
-    const editButtons = document.querySelectorAll('.editBtn');
-    const closeEditButtons = document.querySelectorAll('.closeEditButton');
-    const editModals = document.querySelectorAll('.modalEdit');
-
+    
     if (editButtons.length > 0 && closeEditButtons.length > 0 && editModals.length > 0) {
         handleMultipleButtons(editButtons, closeEditButtons, editModals);
     }
 
-    // Elements for delete modals
-    const deleteButtons = document.querySelectorAll('.removeBtn');
-    const closeDeleteButtons = document.querySelectorAll('.closeRemoveBtn');
-    const deleteModals = document.querySelectorAll('.modalDelete');
 
     if (deleteButtons.length > 0 && closeDeleteButtons.length > 0 && deleteModals.length > 0) {
         handleMultipleButtons(deleteButtons, closeDeleteButtons, deleteModals);
